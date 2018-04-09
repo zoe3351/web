e63e187f70b1d29a8623f710d9e3097993f4cded
736038700@qq.com <736038700@qq.com>
2018-04-09 11:23:52 -0700

    enable login

--- script.js
+++ script.js
@@ -1,117 +1,144 @@
-var app = angular.module('catApp', [
-    'ngRoute'
-]);
-
+var app = angular.module("catApp", ["ngRoute"]);
 
 app.config(function config($routeProvider) {
-    $routeProvider.
-        when('/', {
-            templateUrl: 'pages/map.html',
-            controller: 'mapController'
-        }).
-        when('/login', {
-            templateUrl: 'pages/login.html',
-            controller: 'loginController'
-        }).
-        when('/register', {
-            templateUrl: 'pages/register.html',
-            controller: 'registerController'
-        }).
-        when('/profile', {
-            templateUrl: 'pages/profile.html',
-            controller: 'profileController'
-        }).
-        when('/grade', {
-            templateUrl: 'pages/map.html',
-            controller: 'gradeController'
-        }).
-        when('/vote', {
-            templateUrl: 'pages/map.html',
-            controller: 'voteController'
-        }).
-        when('/display', {
-            templateUrl: 'pages/map.html',
-            controller: 'displayController'
-        }).
-
-        otherwise('/');
+  $routeProvider
+    .when("/", {
+      templateUrl: "pages/map.html",
+      controller: "mapController"
+    })
+    .when("/login", {
+      templateUrl: "pages/login.html",
+      controller: "loginController"
+    })
+    .when("/register", {
+      templateUrl: "pages/register.html",
+      controller: "registerController"
+    })
+    .when("/profile", {
+      templateUrl: "pages/profile.html",
+      controller: "profileController"
+    })
+    .when("/grade", {
+      templateUrl: "pages/map.html",
+      controller: "gradeController"
+    })
+    .when("/vote", {
+      templateUrl: "pages/map.html",
+      controller: "voteController"
+    })
+    .when("/display", {
+      templateUrl: "pages/map.html",
+      controller: "displayController"
+    })
+    .otherwise("/");
 });
 
-app.controller("banController", function ($scope) {
-    $scope.message = 'ban';
+app.controller("banController", function($scope) {
+  $scope.message = "ban";
 });
 
+app.controller("mapController", function($scope, $http) {
+  $scope.message = "map";
+  $http.get("data/proposals.json").then(function(response) {
+    $scope.proposals = response.data;
+  });
 
-app.controller("mapController", function ($scope, $http) {
-    $scope.message = 'map';
-    $http.get('data/proposals.json').then(function (response) {
-        $scope.proposals = response.data;
-    });
+  $scope.showNewProposal = false;
 
+  $scope.showNewProposalTab = () => {
+    $scope.showNewProposal = true;
+  };
+
+  $scope.showProposalListTab = () => {
     $scope.showNewProposal = false;
+  };
 
-    $scope.showNewProposalTab = () => {
-        $scope.showNewProposal = true;
-    }
+  $scope.modal = false;
+  $scope.openModal = () => {
+    $scope.modal = true;
+  };
 
-    $scope.showProposalListTab = () => {
-        $scope.showNewProposal = false;
-    }
-
+  $scope.closeModal = () => {
     $scope.modal = false;
-    $scope.openModal = () => {
-        $scope.modal = true;
-    }
+  };
 
-    $scope.closeModal = () => {
-        $scope.modal = false;
-    }
+  $scope.newProposal = {
+    title: "",
+    idea: "",
+    location: "",
+    why: "",
+    benefit: ""
+  };
 
-    $scope.newProposal = {
+  $scope.submitForm = function(isValid) {
+    if (isValid) {
+      $scope.newProposal.id = Date.now();
+      console.log($scope.newProposal);
+      $scope.newProposal = {
         title: "",
         idea: "",
         location: "",
         why: "",
         benefit: ""
+      };
     }
-
-    $scope.submitForm = function (isValid) {
-        if (isValid) {
-            $scope.newProposal.id = Date.now();
-            console.log($scope.newProposal)
-            $scope.newProposal = {
-                title: "",
-                idea: "",
-                location: "",
-                why: "",
-                benefit: ""
-            }
-
-        }
-    };
+  };
 });
 
-app.controller("loginController", function ($scope) {
-    $scope.message = 'login';
+app.controller("loginController", function($scope, $http) {
+  $scope.message = "login";
+
+  let config = {
+    method: "POST",
+    url: "http://bulubulu.ischool.uw.edu/auth/login",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With"
+    },
+    data: {
+      username: $scope.username,
+      password: $scope.password
+    }
+  };
+  // alert("start login!");
+  $http(config)
+    .success(function(data, status, headers, config) {
+      $scope.PostDataResponse = data;
+    })
+    .error(function(data, status, header, config) {
+      $scope.ResponseDetails =
+        "Data: " +
+        data +
+        "<hr />status: " +
+        status +
+        "<hr />headers: " +
+        header +
+        "<hr />config: " +
+        config;
+    });
+  //   .then(function(response) {
+  //     $scope.proposals = response.data;
+  //   });
 });
 
-app.controller("registerController", function ($scope) {
-    $scope.message = 'register';
+app.controller("registerController", function($scope) {
+  $scope.message = "register";
 });
 
-app.controller("profileController", function ($scope) {
-    $scope.message = 'profile';
+app.controller("profileController", function($scope) {
+  $scope.message = "profile";
 });
 
-app.controller("gradeController", function ($scope) {
-    $scope.message = 'grade';
+app.controller("gradeController", function($scope) {
+  $scope.message = "grade";
 });
 
-app.controller("voteController", function ($scope) {
-    $scope.message = 'vote';
+app.controller("voteController", function($scope) {
+  $scope.message = "vote";
 });
 
-app.controller("displayController", function ($scope) {
-    $scope.message = 'display';
+app.controller("displayController", function($scope) {
+  $scope.message = "display";
 });
-
