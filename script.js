659411486412151d2031e216a9feb53a5245ff88
dongjin <jd62@uw.edu>
2018-04-20 14:14:38 -0700

    move draft to draftmgt page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var app = angular.module("catApp", ["ngRoute", "xeditable"]);
 
 // modify this later
-const SERVER = "http://bulubulu.ischool.uw.edu:4000/";
+const SERVER = "http://localhost:8080/";
 
 app.config(function config($routeProvider) {
     $routeProvider
@@ -33,6 +33,10 @@ app.config(function config($routeProvider) {
         .when("/admin", {
             templateUrl: "pages/admin.html",
             controller: "adminController",
+        })
+        .when("/draftmgt", {
+            templateUrl: "pages/draftmgt.html",
+            controller: "draftmgtController",
         })
         .otherwise("/home");
 });
@@ -811,4 +815,56 @@ app.controller("adminController", function ($scope, $filter, $http, $anchorScrol
             return "invalid!";
         }
     }
+});
+
+app.controller("draftmgtController", function ($scope, $filter, $http, $anchorScroll, $location, $route, $window, $rootScope, DataService) {
+    $scope.allDraft = [];
+
+    let errCallback = function (data, status, header, config) {
+        alert(JSON.stringify(data));
+    }
+
+    DataService.getAllDraft(function (response) {
+        $scope.allDraft = response.data.data;
+    }, errCallback);
+
+    // draft mgt part
+    $scope.saveDraft = function (data, id) {
+        let body = {
+            draft_id: id,
+            proposal_title: data.proposal_title,
+            proposal_idea: data.proposal_idea,
+            project_location: data.project_location,
+            proposal_latitude: data.proposal_latitude,
+            proposal_longitude: data.proposal_longitude
+        }
+
+        $http.post(SERVER + 'draft/edit/' + id, body)
+            .success((data, status, headers, config) => {
+                alert("Draft updated!")
+            })
+            .error(errCallback);
+    };
+
+    $scope.removeDraft = function (id) {
+        if (!confirm("Do you want to DELETE draft ID: " + id + "?")) return;
+        $http.post(SERVER + 'draft/rm/' + id)
+            .success((data, status, headers, config) => {
+                alert("Draft Removed!")
+                $http.get(SERVER + 'draft/all').then(function (res) {
+                    $scope.allDraft = res.data.data;
+                });
+            })
+            .error(errCallback);
+    };
+
+    $scope.check = function (data) {
+        if (!data) {
+            return "invalid!";
+        }
+    };
+
+
+    
+
 });
